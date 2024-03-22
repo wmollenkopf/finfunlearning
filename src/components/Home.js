fa1a556e3adea1ddc2953eaa32ca2de10e14152f
sgrace722 <145815410+sgrace722@users.noreply.github.com>
2024-03-21 22:47:40 -0400

    Working on a HOME Page...

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Home = () => {
-  return <h2>Home Page</h2>;
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <img src="Homescreen.jpg" alt="FinFun Image" />
+      <p>Our goal is to...</p>
+    </div>
+  );
 };
 
 export default Home;
